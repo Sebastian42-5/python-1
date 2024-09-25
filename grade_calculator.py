d99ccf0195afd154f9cb92f45d840b310720a5b1
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-09-25 17:21:15 -0400

    summary of change

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,6 +1,15 @@
-lab_programming = int(input("Enter the number of assignments completed: ")
+labs = 
 quizzes_or_in_class_activities = int(input("Enter the number of quizzes completed: "))
+
 assignment_1 = int(input("Enter grade for Assignment 1: "))
 assignment_2 = int(input("Enter grade for Assignment 2: "))
 assignment_3 = int(input("Enter grade for Assignment 3: "))
 assignment_4 = int(input("Enter grade for Assignment 4: "))
+midterm_1 = int(input("Enter grade for Midterm 1: "))
+midterm_2 = int(input("Enter grade for Midterm 2: "))
+final_exam = int(input("Enter grade for Final Exam: "))
+midterms_and_final_prep = int(input("Enter grade for Midterms and Final Preparation: "))
+
+# calculations 
+
+
