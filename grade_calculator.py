0e0ac60151aafce69eef3223e7207db44b264864
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-09-25 23:44:24 -0400

    summary of change

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,6 +1,7 @@
-labs = 
-quizzes_or_in_class_activities = int(input("Enter the number of quizzes completed: "))
+# Input from the user
 
+lab_problems = int(input("Enter the number of labs completed: "))
+quizzes_or_in_class_activities = int(input("Enter the number of quizzes completed: "))
 assignment_1 = int(input("Enter grade for Assignment 1: "))
 assignment_2 = int(input("Enter grade for Assignment 2: "))
 assignment_3 = int(input("Enter grade for Assignment 3: "))
@@ -10,6 +11,11 @@ midterm_2 = int(input("Enter grade for Midterm 2: "))
 final_exam = int(input("Enter grade for Final Exam: "))
 midterms_and_final_prep = int(input("Enter grade for Midterms and Final Preparation: "))
 
-# calculations 
+# Grade calculation 
 
+lab_weigh = int(lab_problems * 20/6) 
+activity_weigh = int(quizzes_or_in_class_activities * 15/6)
 
+overall_grade = ((int(lab_weigh + activity_weigh + (assignment_1 + assignment_2 + assignment_3 + assignment_4) * 16 + (midterm_1 + midterm_2) * 25 + final_exam * 18 + midterms_and_final_prep * 6)) / 100)
+
+print("Your grade is: ", overall_grade)
