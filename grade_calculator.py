a7c2ffffdfc9c3bf2063a35842db4b793d458b1e
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-09-27 23:40:06 -0400

    summary of changes

--- grade_calculator.py
+++ grade_calculator.py
@@ -23,6 +23,6 @@ if quizzes_or_in_class_activities > 6:
 else:
     activity_percent = int(quizzes_or_in_class_activities * 100/6)
 
-overall_grade = int(((lab_percent * 20) + (activity_percent * 15) + (assignment_1 * 4) + (assignment_2 * 4) + (assignment_3 * 4) + (assignment_4 * 4) + (midterm_1 * 12.5) + (midterm_2 * 12.5) + (final_exam * 18) + (midterms_and_final_prep * 6)) / 100)
+overall_grade = round(((lab_percent * 20) + (activity_percent * 15) + (assignment_1 * 4) + (assignment_2 * 4) + (assignment_3 * 4) + (assignment_4 * 4) + (midterm_1 * 12.5) + (midterm_2 * 12.5) + (final_exam * 18) + (midterms_and_final_prep * 6)) / 100)
 
 print("Your grade is: ", overall_grade)
