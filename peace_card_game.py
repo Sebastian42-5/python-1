1c226073ddeefc66f1b725200e675ea07049e58d
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-10-09 21:19:14 -0400

    summary of changes

--- peace_card_game.py
+++ peace_card_game.py
@@ -19,9 +19,9 @@ random.shuffle(deck)
 
 # Split the deck into two hands
 
-deck_1 = deck[:26]
+p1_deck = deck[:26]
 
-deck_2 = deck[26:]
+p2_deck = deck[26:]
 
 p1_card = p1_deck.pop(0)
 p2_card = p2_deck.pop(0)
@@ -29,28 +29,41 @@ p2_card = p2_deck.pop(0)
 
 def card_comparison(p1_card, p2_card):
     if ranks.index(p1_card[0]) > ranks.index(p2_card[0]): 
-        print("player 1 has a stronger card") 
+        print("Player 1 has a stronger card! They win!") 
         return 1
-    if ranks.index(p1_card[0]) < ranks.index(p2_card[0]):
-        print("player 2 has a stronger card") 
+    elif ranks.index(p1_card[0]) < ranks.index(p2_card[0]):
+        print("Player 2 has a stronger card! They win!") 
         return 2
-    return 0
-
-result = card_comparison(p1_card , p2_card)
-if result == 1: 
-    p1_deck.append(p1_card)
-    p1_deck.append(p2_card)
+    else :
+        return 0
 
 
-    """This is the logic that compares two cards to find the stronger card
-		Return 1 if player 1's card is strong, 2 for player 2
+    """this is the logic that compares two cards to find the stronger card
+		return 1 if player 1's card is strong, 2 for player 2
 		if the cards are equal, return 0.
 
-		Hint, using the index function will make this very simple (one liner)"""
-    # Your code here
+		hint, using the index function will make this very simple (one liner)"""
+    # your code here
 
 def play_round(player1_hand, player2_hand):
-    if :
+    
+    p1_card = p1_deck.pop(0)
+    p2_card = p2_deck.pop(0)
+    
+    result = card_comparison(p1_card , p2_card)
+
+    if result == 1: 
+        p1_deck.append(p1_card)
+        p1_deck.append(p2_card)
+
+    if result == 2:
+        p2_deck.append(p2_card)
+        p2_deck.append(p1_card)
+
+    else:
+        print("There is a tie! That means War!")
+        war(player_1hand, player_hand)
+
     """Play a single round of the game.
 		That is, each player flips a card, and the winner is determined using the card_comparison function
 		if both players flip the same value card, call the war function
@@ -58,6 +71,28 @@ def play_round(player1_hand, player2_hand):
     # Your code here
 
 def war(player1_hand, player2_hand):
+    for deck in range(4):
+
+        p1_hidden_cards = p1_deck.pop(0) 
+        p2_hidden_cards = p1_deck.pop(0)
+    p1_war_card = p1_hidden_cards[-1]
+    p2_war_card = p2_hidden_cards[-1]
+
+    if result == 1:
+       p1_deck.append(p1_war_card)
+       p1_deck.append(p2_war_card)
+    
+    if result == 2:
+       p2_deck.append(p2_war_card)
+       p1_deck.append(p1_war_card)
+    else :
+        print("The war continues!")
+        war(player1_hand, player2_hand)
+
+
+
+
+    
     """Handle the 'war' scenario when cards are equal.
 		recall the rules of war, both players put 3 cards face down, 
 		then both players flip face up a 4th card. The player with the stronger
@@ -66,15 +101,17 @@ def war(player1_hand, player2_hand):
     # Your code here
 
 def play_game():
+
+
+    
     """Main function to run the game."""
     # Your code here
+
+
 def gameloop():
     while len(p1_deck) > 0 and len(p2_deck) > 0:
             
-result = card_comparison(p1_card , p2_card)
-if result == 1: 
-    p1_deck.append(p1_card)
-    p1_deck.append(p2_card)
+
 # Call the main function to start the game
 play_game()
 
